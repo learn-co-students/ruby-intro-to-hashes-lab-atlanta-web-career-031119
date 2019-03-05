9108e66e79d5b4590cd9640a194d101bdadfdcea
Travis Martin <travisemerymartin@gmail.com>
2019-03-05 19:43:19 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,13 +13,13 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-  monopoly=Hash.new
-  monopoly[:railroads]={}
-	monopoly[:railroads][:pieces]=4
+  monopoly={railroads:{}}
+  monopoly[:railroads][:pieces]=4
 end
 
 def monopoly_with_third_tier
-  monopoly[:railroads]<<{names: {}}
+  monopoly={railroads:{pieces:4}}
+  monopoly[:railroads][]
 end
 
 def monopoly_with_fourth_tier
